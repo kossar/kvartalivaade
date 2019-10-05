656286eb3dffe3f18496107acb3345972a610014
Ott <ott.kossar@gmail.com>
2019-10-05 09:49:29 +0300

    Updates

---
+++ precache-manifest.a974f3d7df0b5ec6a40d18282d909214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3b5a1cd03aa09e3f68ab2d7a514672",
+    "url": "/kvartalivaade/index.html"
+  },
+  {
+    "revision": "d6ad720144b0be2b81fd",
+    "url": "/kvartalivaade/static/css/main.42ef6cf2.chunk.css"
+  },
+  {
+    "revision": "13a28ac0ea8caf433944",
+    "url": "/kvartalivaade/static/js/2.26544584.chunk.js"
+  },
+  {
+    "revision": "d6ad720144b0be2b81fd",
+    "url": "/kvartalivaade/static/js/main.670a7eb2.chunk.js"
+  },
+  {
+    "revision": "ad2e02b540789eb35a94",
+    "url": "/kvartalivaade/static/js/runtime-main.eb3d8f9d.js"
+  }
+]);
